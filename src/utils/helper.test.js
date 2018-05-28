143ffd85d0dc054e7a35847331f0e1af7d84ab94
Kai Hotz <kaihotz@me.com>
2018-05-28 22:03:36 +0200

    improved helper test

--- src/utils/helper.test.js
+++ src/utils/helper.test.js
@@ -2,7 +2,6 @@ import { getFormattedValue } from './helper'
 
 describe('Format value', () => {
   it('should work', () => {
-    const value = '5.7'
-    expect(getFormattedValue(value)).toBe(value.toLocaleString())
+    expect(getFormattedValue('5.7')).toBe('5.7')
   })
 })
