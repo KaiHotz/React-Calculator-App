c6feaa4af3ae0716bfa9db223fd8f92f64763950
Kai Hotz <kaihotz@me.com>
2018-11-01 21:56:19 +0100

    Updates

---
+++ precache-manifest.336f38340818578b88d96ba0ad5a75ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72094e7399fb3e014b3a",
+    "url": "/React-Calculator-App/static/js/runtime~main.72094e73.js"
+  },
+  {
+    "revision": "966311628a0c127b0657",
+    "url": "/React-Calculator-App/static/js/main.96631162.chunk.js"
+  },
+  {
+    "revision": "aa7f3fab423e0567b404",
+    "url": "/React-Calculator-App/static/js/1.aa7f3fab.chunk.js"
+  },
+  {
+    "revision": "966311628a0c127b0657",
+    "url": "/React-Calculator-App/static/css/main.f3490656.chunk.css"
+  },
+  {
+    "revision": "553b5d3df275ad9551a5892819a29c70",
+    "url": "/React-Calculator-App/index.html"
+  }
+];
