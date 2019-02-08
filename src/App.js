0a8aed9fc7250984ce3ae45f0eea4b797cd34f8b
khotz <kai.hotz@complevo.de>
2019-02-08 14:48:59 +0100

    React update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Calculator from './components/Calculator'
 import './App.css'
 
-class App extends Component {
-  render () {
-    return (
-      <div className="app">
-        <Calculator />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className="app">
+    <Calculator />
+  </div>
+)
 
 export default App
