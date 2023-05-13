854b1dc7339e3a279a5538fb32321edd168d42ca
KaiHotz <kaihotz@me.com>
2023-05-13 18:36:00 +0400

    adjustments

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-Calculator-App/',
   test: {
     globals: true,
     environment: 'jsdom',
